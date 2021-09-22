24d0af8875f509e49838eaa1e1f5bffd52ec4080
Cacho Carro <lcarro@globalnewsgroup.com>
2021-09-22 17:57:58 -0300

    agrego los archivos

---
+++ modulos/modulo.py
@@ -0,0 +1,15 @@
+import random
+
+def from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code"""
+    return "#%02x%02x%02x" % rgb
+
+def save_data():
+    print(
+        "Guardando"
+    )
+
+def get_data():
+        print(
+        "Recuperando"
+    )
