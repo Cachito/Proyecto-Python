2fea84337c4dc2b627fe02dbedde31e238774390
Cacho Carro <lcarro@globalnewsgroup.com>
2021-09-27 16:22:49 -0300

    menú principal
    pantalla de menú, incluye about y menues cque abre solo dialogos informativos.

--- modulos/modulo.py
+++ modulos/modulo.py
@@ -1,8 +1,4 @@
-import random
-
-def from_rgb(rgb):
-    """translates an rgb tuple of int to a tkinter friendly color code"""
-    return "#%02x%02x%02x" % rgb
+from tkinter.messagebox import *
 
 def save_data():
     print(
@@ -13,3 +9,18 @@ def get_data():
         print(
         "Recuperando"
     )
+
+def about():
+    showinfo("Entrega Intermedia", "Cargador de Noticias\n\nGrupo:\n- Luis Carro\n- Cristian Maier")
+
+def abm_medios():
+    showinfo("Entrega Intermedia", "ABM de Medios")
+
+def abm_divisiones():
+    showinfo("Entrega Intermedia", "ABM de Divisiones")
+
+def cargador():
+    showinfo("Entrega Intermedia", "Carga de Noticias")
+
+def hola():
+    print("Hola!")    
