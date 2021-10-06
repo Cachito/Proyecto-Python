c8b8ddab33aa826bdcbbda75e4dea3ce185e3dfb
Cacho Carro <lcarro@globalnewsgroup.com>
2021-10-06 18:49:04 -0300

    cambios para pruebas

--- Consulta/main_1.py
+++ Consulta/main_1.py
@@ -1,9 +1,14 @@
+import sys
 from tkinter import *
+
+sys.path.append("../Proyecto-Python/modulos")
+
+from modulo import *
 
 root = Tk()
 
 width = 425
-height = 150
+height = 300
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -11,38 +16,19 @@ screen_height = root.winfo_screenheight()
 x = (screen_width / 2) - (width / 2)
 y = (screen_height / 2) - (height / 2)
 
-root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+#root.geometry("%dx%d+%d+%d" % (width, height, x, y))
 
-root.minsize(width, height)
-root.maxsize(width, height)
+#root.minsize(width, height)
+#root.maxsize(width, height)
 
 root.title("Proyecto")
 
-lbl_titulo = Label(
-    root, text="Ingrese datos", bg="DarkOrchid3", fg="thistle1", height=1, width=60
-)  # válido
-lbl_titulo.grid(row=0, column=0, padx=1, pady=1, sticky=W + E, columnspan=4)
+root.iconbitmap("./imagenes/noticias.ico")
 
-titulo_val = StringVar()
-fecha_val = StringVar()
-cuerpo_val = StringVar()
+lbl_titulo = Label(text="Título")
+val_titulo = Entry()
 
-Label(root, text="Título").grid(row=2, column=0, sticky=W)
-Label(root, text="Fecha").grid(row=4, column=0, sticky=W)
-Label(root, text="Cuerpo").grid(row=6, column=0, sticky=W)
+lbl_titulo.pack()
+val_titulo.pack()
 
-titulo = Entry(root, textvariable=titulo_val)
-fecha = Entry(root, textvariable=fecha_val)
-cuerpo = Entry(root, textvariable=cuerpo_val)
-
-titulo.grid(row=2, column=1)
-fecha.grid(row=4, column=1)
-cuerpo.grid(row=6, column=1)
-
-btn_save = Button(root, text="Guardar")
-btn_save.grid(row=8, column=1, sticky=W + E)
-
-btn_get = Button(root, text="Traer")
-btn_get.grid(row=8, column=2, sticky=W + E)
-
-mainloop()
+root.mainloop()
--- modulos/modulo.py
+++ modulos/modulo.py
@@ -11,28 +11,5 @@ def cancel_medio():
 def about():
     showinfo("Entrega Intermedia", "Cargador de Noticias\n\nGrupo:\n- Luis Carro\n- Cristian Maier")
 
-def abm_medios(root):
-    medios_window = tkinter.Toplevel(root)
-    medios_window.title("ABM de Medios")
-
-    nombre_val = StringVar()
-    Label(medios_window, text="Nombre").grid(row=2, column=0, sticky=W)
-    nombre = Entry(medios_window, textvariable=nombre_val)
-    nombre.grid(row=2, column=1)
-
-    btn_save = Button(medios_window, text="Guardar", command = save_medio(nombre_val.get()))
-    btn_save.grid(row=8, column=1)
-
-    btn_cancel = Button(medios_window, text="Cancelar", command = cancel_medio())
-    btn_cancel.grid(row=8, column=2)
-
-    medios_window.mainloop()
-
-def abm_divisiones():
-    showinfo("Entrega Intermedia", "ABM de Divisiones")
-
-def cargador():
-    showinfo("Entrega Intermedia", "Carga de Noticias")
-
 def hola():
     print("Hola!")    
